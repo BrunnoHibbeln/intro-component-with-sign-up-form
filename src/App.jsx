2b071e4d52c02aa666c7b873fe1b4df11af296b1
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-09-21 22:54:08 +0000

    adding `fonts config`

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <main></main>
-}
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 
-export default App
+import Article from "./components/Article";
+import Form from "./components/Form";
+
+export default function App() {
+  return (
+    <main className="flex min-h-screen flex-col gap-14 bg-red bg-mobile px-5 py-14 font-poppins duration-300 sm:px-28 lg:flex-row lg:items-center lg:justify-center lg:gap-10">
+      <Article />
+      <Form />
+    </main>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "hsl(0, 100%, 74%) ",
+        green: "hsl(154, 59%, 51%)",
+        "grayish-blue": "hsl(246, 25%, 77%)",
+        blue: "hsl(248, 32%, 49%)",
+        "dark-blue": "hsl(249, 10%, 26%)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontSize: { body: "1rem" },
+      backgroundImage: {
+        mobile: ["url(./src/assets/bg-intro-mobile.png)"],
+        desktop: ["url(./src/assets/bg-intro-desktop.png)"],
+      },
+    },
   },
   plugins: [],
 };
