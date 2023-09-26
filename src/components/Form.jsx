92963b92ab3bd73b8f9deabd36409878aff7185c
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-09-26 19:41:10 +0000

    adding `form validation` with `Formik` and `Yup`

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,18 @@
+import * as Yup from "yup";
+import { Formik, Form as FormikForm, useField } from "formik";
 import Input from "./Input";
+import { useState } from "react";
 
 export default function Form() {
+  const [field, meta] = useField();
+  const [inputProps, setInputProps] = useState("border");
+
+  const handleInputError = () => {
+    if (meta.touched && meta.error) {
+      setInputProps("border-2 border-red");
+    } else null;
+  };
+
   return (
     <section className="flex flex-col gap-6 lg:w-2/5">
       <section className="rounded-lg bg-blue px-14 py-4 text-center text-white shadow-2xl">
@@ -8,22 +20,50 @@ export default function Form() {
           <strong>Try it free 7 days</strong> then $20/mo. thereafter
         </p>
       </section>
-      <form className="lg:2/4 flex flex-col items-center justify-start gap-4 rounded-lg bg-white px-5 py-7 lg:px-8 lg:py-9">
-        <Input placeholder="First Name" />
-        <Input placeholder="Last Name" />
-        <Input placeholder="Email Address" />
-        <Input placeholder="Password" />
-        <button
-          className="w-full rounded-md bg-green p-5 text-center font-medium text-white shadow-2xl duration-300 hover:brightness-110 lg:p-3"
-          type="submit"
-        >
-          CLAIM YOUR FREE TRIAL
-        </button>
-        <small className="font-xs px-5 text-center text-grayish-blue">
-          By clicking the button, you are agreeing to our
-          <strong className="text-red"> Terms and Services</strong>
-        </small>
-      </form>
+
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+        }}
+        validationSchema={Yup.object({
+          firstName: Yup.string().required("First Name cannot be empty"),
+          lastName: Yup.string().required("Last Name cannot be empty"),
+          email: Yup.string()
+            .email("Looks like this is not an email")
+            .required("Email cannot be empty"),
+          password: Yup.string().required("Password cannot be empty"),
+        })}
+        onSubmit={async (values, { setSubmitting }) => {
+          await new Promise((r) => setTimeout(r, 500));
+          setSubmitting(false);
+        }}
+      >
+        <FormikForm className="relative flex flex-col items-center justify-start gap-4 rounded-lg bg-white px-5 py-7 lg:px-8 lg:py-9">
+          <Input
+            inputProps={inputProps}
+            name="firstName"
+            type="text"
+            placeholder="First Name"
+          />
+          <Input name="lastName" type="text" placeholder="Last Name" />
+          <Input name="email" type="email" placeholder="Email Address" />
+          <Input name="password" type="password" placeholder="Password" />
+
+          <button
+            className="w-full rounded-md bg-green p-5 text-center font-medium text-white shadow-2xl duration-300 hover:brightness-110 lg:p-3"
+            type="submit"
+          >
+            CLAIM YOUR FREE TRIAL
+          </button>
+          <small className="font-xs px-5 text-center text-grayish-blue">
+            By clicking the button, you are agreeing to our
+            <strong className="text-red"> Terms and Services</strong>
+          </small>
+        </FormikForm>
+      </Formik>
     </section>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,28 @@
-export default function Input({ placeholder }) {
+import { useField } from "formik";
+import IconError from "../assets/icon-error.svg";
+
+export default function Input({ inputProps, placeholder, ...props }) {
+  const [field, meta] = useField(props);
+
   return (
-    <input
-      className="w-full rounded-md border border-grayish-blue p-5 font-semibold lg:px-8 lg:py-3"
-      placeholder={placeholder}
-      type="text"
-    ></input>
+    <section className="relative w-full">
+      <input
+        className={`w-full rounded-md ${inputProps} p-5 font-semibold lg:px-8 lg:py-3`}
+        {...field}
+        {...props}
+      ></input>
+
+      {meta.touched && meta.error ? (
+        <>
+          <img
+            className="absolute right-8 top-5 lg:top-3.5"
+            src={IconError}
+          ></img>
+          <p className="pt-2 text-right text-xs font-semibold italic text-red">
+            {meta.error}
+          </p>
+        </>
+      ) : null}
+    </section>
   );
 }
